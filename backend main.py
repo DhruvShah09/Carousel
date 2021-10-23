0b85a4b22a36c45ee2aba083159c7fc230e5d7f9
thex23 <jamesjkeller@yahoo.com>
2021-10-22 21:47:55 -0400

    csv main+

--- backend main.py
+++ backend main.py
@@ -1,12 +1,14 @@
 import numpy as np
+import pandas as pd
+
+data = pd.read_csv(r'')
 
 class User:
-    username = ""
-    password = ""
-    name = ""
-    year = 0
-    def __init__(self,username,password,name,year):
+    def __init__(self,id,username,password,name,year):
+        self.id = id
         self.username = username
         self.password = password
         self.name = name
         self.year = year
+
+    def __init__(self,id):
